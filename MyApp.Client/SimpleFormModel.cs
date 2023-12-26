28b25b87d8f5e339ba2293d9af334f586f09a4d5
suhut <suhut.wadiyo@gmail.com>
2023-12-27 06:32:19 +0700

    simple form

---
+++ MyApp.Client/SimpleFormModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp.Client;
+
+public class SimpleFormModel
+{
+    public int Id { get; set; }
+    public string IduTextBox { get; set; } = string.Empty;
+    public string IduTextBoxEmail { get; set; } = string.Empty;
+    public string? IduTextArea { get; set; }
+    public int? IduInt { get; set; }
+    public decimal? IduDecimal { get; set; }
+
+}
