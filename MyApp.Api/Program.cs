4076762d400a8cb58319003a2952e091492db455
suhut <suhut.wadiyo@gmail.com>
2023-12-27 14:30:58 +0700

    DataGrid

---
+++ MyApp.Api/Program.cs
@@ -0,0 +1,70 @@
+using MyApp.Shared;
+using System.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    // this defines a CORS policy called "default"
+    options.AddPolicy("default", policy =>
+    {
+        policy.WithOrigins("https://localhost:44398")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    GridDataRequestDto request = new();
+    request.Page = 0;
+    request.PageSize = 10;
+
+    WeatherListDto result = new();
+
+    var items = Enumerable.Range(1, 1000).Select(index =>
+        new WeatherListItemDto
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }
+        )
+        .ToList();
+
+    result.ItemTotalCount = items.Count;
+    result.Items = items.Skip(request.PageSize* request.PageSize).Take(request.PageSize).ToList();
+
+    return result;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.UseCors("default");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ MyApp.Shared/Weather.cs
@@ -0,0 +1,21 @@
+﻿namespace MyApp.Shared;
+
+public class GridDataRequestDto
+{
+    public int Page { get; set; } = 0; // The page number for the data we're requesting
+    public int PageSize { get; set; } = 10; // The number of items per page
+}
+
+public class WeatherListDto
+{
+    public List<WeatherListItemDto> Items { get; set; } = new();
+    public int ItemTotalCount { get; set; } = 0; // The total count of items before paging
+}
+
+public class WeatherListItemDto
+{
+    public DateOnly Date { get; set; } 
+    public int TemperatureC { get; set; } 
+    public string? Summary { get; set; } 
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
