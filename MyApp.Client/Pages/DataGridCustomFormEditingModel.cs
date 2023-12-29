2fb8951dda2bacb828f63cb97271320e1b5f8e55
suhut <suhut.wadiyo@gmail.com>
2023-12-29 07:35:28 +0700

    custom form editing

---
+++ MyApp.Client/Pages/DataGridCustomFormEditingModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MyApp.Client.Pages;
+
+public class DataGridCustomFormEditingModel
+{
+    [JsonPropertyName("date")]
+    public DateTime? Tanggal { get; set; }
+
+    public int? TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+    public decimal? TemperatureF => (32.0m + (TemperatureC / 0.5556m));
+}
