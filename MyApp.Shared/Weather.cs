b9383d4966e23161ef66997d53b638f52d887efa
suhut <suhut.wadiyo@gmail.com>
2024-01-02 09:07:39 +0700

    filter

--- MyApp.Shared/Weather.cs
+++ MyApp.Shared/Weather.cs
@@ -1,4 +1,6 @@
-﻿namespace MyApp.Shared;
+﻿using System.Text;
+
+namespace MyApp.Shared;
 
 public class GridDataRequestDto
 {
@@ -10,21 +12,146 @@ public class GridDataRequestDtov03
 {
     public int Page { get; set; } = 0; // The page number for the data we're requesting
     public int PageSize { get; set; } = 10; // The number of items per page
-    public List<GridDataRequestSortDefinitionsv03>? SortDefinitions { get; set; } 
-    public List<GridDataRequestFilterDefinitionsv03>? FilterDefinitions { get; set; } 
+    public List<GridDataRequestSortDefinitionsv03>? SortDefinitions { get; set; }
+    public List<GridDataRequestFilterDefinitionsv03>? FilterDefinitions { get; set; }
 }
 
 public class GridDataRequestSortDefinitionsv03
 {
-    public string SortBy { get; set; }  
-    public bool Descending { get; set; } 
+    public string SortBy { get; set; }
+    public bool Descending { get; set; }
 }
 
 public class GridDataRequestFilterDefinitionsv03
 {
-    public string Field { get; set; }
+    public string FieldName { get; set; }
+    public string FieldType { get; set; }
     public string Operator { get; set; }
     public object Value { get; set; }
+}
+
+public class GridDataRequestUtils03
+{
+    public string SortDefinitionsToSql(GridDataRequestDtov03 request)
+    {
+        StringBuilder result = new StringBuilder();
+        foreach (var item in request.SortDefinitions ?? [])
+        {
+            var fieldName = item.SortBy;
+
+            var statement = item.Descending switch
+            {
+                true => $"{fieldName} DESC ",
+                false => $"NOT({fieldName} ASC "
+            }; 
+
+            result = result.Append(" AND ");
+            result = result.Append(statement);
+
+
+        }
+        return result.ToString();
+    }
+
+    public string FilterDefinitionsToSql(GridDataRequestDtov03 request)
+    {
+        StringBuilder result = new StringBuilder();
+
+        foreach (var item in request.FilterDefinitions ?? [])
+        {
+            var fieldName = item.FieldName;
+            var fieldType = item.FieldType;
+            var fieldValue = item.Value;
+
+            var statement = string.Empty;
+            if (fieldType == "String")
+            {
+                statement = item.Operator switch
+                {
+                    "contains" => $"{fieldName} LIKE '%{fieldValue}%'",
+                    "not contains" => $"NOT({fieldName} LIKE '%{fieldValue}%')",
+                    "equals" => $"{fieldName}='{fieldValue}'",
+                    "not equals" => $"{fieldName}!='{fieldValue}'",
+                    "starts with" => $"{fieldName} LIKE '%{fieldValue}'",
+                    "ends with" => $"{fieldName} LIKE '{fieldValue}%'",
+                    "is empty" => $"ISNULL({fieldName},'')=''",
+                    "is not empty" => $"ISNULL({fieldName},'')!=''",
+                    _ => string.Empty
+                };
+            }
+            else if (fieldType == "Number")
+            {
+                statement = item.Operator switch
+                {
+                    "=" => $"{fieldName}={fieldValue}",
+                    "!=" => $"{fieldName}!={fieldValue}",
+                    ">" => $"{fieldName}>{fieldValue}",
+                    ">=" => $"{fieldName}>={fieldValue}",
+                    "<" => $"{fieldName}<{fieldValue}",
+                    "<=" => $"{fieldName}<={fieldValue}",
+                    "is empty" => $"{fieldName} IS NULL ",
+                    "is not empty" => $"{fieldName} IS NOT NULL ",
+                    _ => string.Empty
+                };
+            }
+            else if (fieldType == "DateTime")
+            {
+                statement = item.Operator switch
+                {
+                    "is" => $"{fieldName}='{fieldValue}'",
+                    "is not" => $"{fieldName}!='{fieldValue}'",
+                    "is after" => $"{fieldName}>'{fieldValue}'",
+                    "is on or after" => $"{fieldName}>='{fieldValue}'",
+                    "is before" => $"{fieldName}<'{fieldValue}'",
+                    "is on or before" => $"{fieldName}<='{fieldValue}'",
+                    "is empty" => $"{fieldName} IS NULL ",
+                    "is not empty" => $"{fieldName} IS NOT NULL ",
+                    _ => string.Empty
+                };
+            }
+            else if (fieldType == "Guid")
+            {
+
+                statement = item.Operator switch
+                {
+                    "equals" => $"{fieldName}='{fieldValue}'",
+                    "not equals" => $"{fieldName}!='{fieldValue}'",
+                    _ => string.Empty
+                };
+            }
+            else if (fieldType == "Boolean")
+            {
+                statement = item.Operator switch
+                {
+                    "is" => $"{fieldName}={fieldValue}",
+                    _ => string.Empty
+                };
+            }
+            else if (fieldType == "Enum")
+            {
+                statement = item.Operator switch
+                {
+                    "is" => $"{fieldName}={fieldValue}",
+                    "is not" => $"{fieldName}!={fieldValue}",
+                    _ => string.Empty
+                };
+            }
+
+
+            if (string.IsNullOrEmpty(statement))
+            {
+                result = result.Append(" AND ");
+                result = result.Append(statement);
+            }
+
+        }
+        return result.ToString();
+    }
+
+    //public string ConvertToLinq(GridDataRequestDtov03 request)
+    //{
+
+    //}
 }
 
 
@@ -38,8 +165,8 @@ public class WeatherListDto
 
 public class WeatherListItemDto
 {
-    public DateTime? Date { get; set; } 
-    public int? TemperatureC { get; set; } 
-    public string? Summary { get; set; } 
+    public DateTime? Date { get; set; }
+    public int? TemperatureC { get; set; }
+    public string? Summary { get; set; }
     public int? TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
